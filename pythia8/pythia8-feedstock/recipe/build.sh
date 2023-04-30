66628cda755b082d3250df76b94b43afbc62303f
Jin-Hwan CHO <jinhwan.cho@gmail.com>
2023-05-01 00:04:56 +0900

    Add pythia8-feedstock for MadGraph5

---
+++ pythia8/pythia8-feedstock/recipe/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "${target_platform}" == linux-* ]]; then
+    PYTHIA_ARCH=LINUX
+else
+    PYTHIA_ARCH=DARWIN
+fi
+
+EXTRAS=""
+
+# Use pybind11 from conda-forge
+sed -i 's@overload_caster_t@override_caster_t@g' plugins/python/src/*.cpp
+rm -rf plugins/python/include/pybind11
+ln -s $PREFIX/include/pybind11 $PWD/plugins/python/include/pybind11
+
+# Copy plugin needed for FxFx
+cp "${RECIPE_DIR}/JetMatching.h" "${PWD}/include/Pythia8Plugins/JetMatching.h"
+
+./configure \
+    --with-python-include="$(python -c "from sysconfig import get_paths; info = get_paths(); print(info['include'])")" \
+    --with-python-bin="${PREFIX}/bin/" \
+    --arch=${PYTHIA_ARCH} \
+    --prefix=${PREFIX} \
+    --with-hepmc2=${PREFIX} \
+    --with-gzip=${PREFIX} \
+    --with-lhapdf6 \
+    --with-lhapdf6-plugin=LHAPDF6.h \
+    ${EXTRAS}
+
+make install -j${CPU_COUNT}
+
+# Make links so conda can find the bindings
+ln -s "${PREFIX}/lib/pythia8.so" "${SP_DIR}/"
+
+# Update ${BUILD_PREFIX} -> ${PREFIX}
+sed -i 's/\/conda-bld\/pythia8_[0-9]*\/_build_env//g' "${PREFIX}/share/Pythia8/examples/Makefile.inc"
